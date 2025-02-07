0bc29df6862fc0b8e3f99336816155f5b848bc26
Nithin-David <nithindavid96@gmail.com>
2025-02-07 17:51:27 +0530

    first commit

---
+++ Server/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => {
+    // Implement your signup logic here
+    res.json({ message: 'User signed up successfully' });
+}
+
+export const login = (req, res) => {
+    // Implement your signup logic here
+    res.json({ message: 'User login successfully' });
+}
+
+export const logout = (req, res) => {
+    // Implement your signup logic here
+    res.json({ message: 'User logout successfully' });
+}
